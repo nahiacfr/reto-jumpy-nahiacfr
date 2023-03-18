3492676e2719420f62e785a78413a84c701f1030
nahiacfr <nahia.castaneda@opendeusto.es>
2023-03-18 16:38:10 +0100

    mover zorro

---
+++ Assets/Move.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody riggidBody;
+    private bool canJump;
+    private bool moveForward;
+    [SerializeField]
+    public int jumpForce = 200;
+    [SerializeField]
+    float moveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        riggidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move1();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            riggidBody.AddForce(Vector3.back);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ground"))
+        {
+            canJump = true;
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+        //other.gameObject.
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+
+    private void Move1()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+}
