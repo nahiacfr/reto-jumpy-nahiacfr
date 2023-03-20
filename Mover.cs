dafa31728f11164f2d5b754d4435e58cebf17196
nahiacfr <nahia.castaneda@opendeusto.es>
2023-03-20 09:40:17 +0100

    Salto hecho

---
+++ Mover.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody riggidBody;
+    [SerializeField]
+    private bool canJump;
+    private bool moveForward;
+    [SerializeField]
+    public int jumpForce = 200;
+    [SerializeField]
+    float moveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        riggidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Jump()
+    {
+    if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            riggidBody.AddForce(Vector3.up*jumpForce);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground") || collision.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            canJump = true;
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+        //other.gameObject.
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground") || collision.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            canJump = false;
+        }
+      
+    }
+
+    private void Move()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+}
